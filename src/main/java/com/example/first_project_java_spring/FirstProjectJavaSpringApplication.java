dcaaebe804b25311326ed13e6e4efd5b71b122a3
Kiril chepel <kchepel1@stu.vistula.edu.pl>
2026-01-15 05:18:01 +0100

    Initial commit

---
+++ src/main/java/com/example/first_project_java_spring/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.first_project_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+    }
+}
